791519c461336e81d6838da3de6994b546529b85
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-04-02 12:29:38 +0200

    test(*): make tests actually work

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,13 +1,21 @@
-
 extern crate readext;
 
-use std::io::{Read,Cursor};
+use std::io::{Cursor};
 use readext::ReadExt;
 
 #[test]
-fn test () {
+fn can_read_into_string () {
     let bytes = b"hello";
-    let mut input = Cursor::new(bytes);
-    let s = input.read_into_string();
+    let mut input = Cursor::new(bytes as &[u8]);
+    let s = input.read_into_string().unwrap();
     assert_eq!(s, "hello");
+}
+
+
+#[test]
+fn can_read_into_vec () {
+    let bytes = b"hello";
+    let mut input = Cursor::new(bytes as &[u8]);
+    let s = input.read_into_vec().unwrap();
+    assert_eq!(s.len(), 5);
 }
