74a4a60ee2437fb8f33ebc77207cd39034159c4d
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-04-02 11:17:46 +0200

    feat(*): adds ReadExt trait

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use std::io::{ Result, Read };
+
+trait ReadExt: Read {
+    /// Read all bytes until EOF in this source, returning them as a new `Vec`.
+    ///
+    /// See `read_to_end` for other semantics.
+    fn read_into_vec(&mut self) -> Result<Vec<u8>> {
+        let mut buf = Vec::new();
+        let res = self.read_to_end(&mut buf);
+        res.map(|_| buf)
+    }
+
+    /// Read all bytes until EOF in this source, returning them as a new buffer.
+    ///
+    /// See `read_to_string` for other semantics.
+    fn read_into_string(&mut self) -> Result<String> {
+        let mut buf = String::new();
+        let res = self.read_to_string(&mut buf);
+        res.map(|_| buf)
+    }
+}
