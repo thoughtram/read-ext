505ff8abfcdf58a58247daba0eb9b5e85fc31f1c
Richard Neumann <neumann@paulmann.de>
2023-10-19 08:59:08 +0200

    Implement ReadExt::read_into_array_exact()

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,21 +1,32 @@
 extern crate readext;
 
-use std::io::{Cursor};
 use readext::ReadExt;
+use std::io::Cursor;
 
 #[test]
-fn can_read_into_string () {
+fn can_read_into_string() {
     let bytes = b"hello";
     let mut input = Cursor::new(bytes as &[u8]);
     let s = input.read_into_string().unwrap();
     assert_eq!(s, "hello");
 }
 
-
 #[test]
-fn can_read_into_vec () {
+fn can_read_into_vec() {
     let bytes = b"hello";
     let mut input = Cursor::new(bytes as &[u8]);
     let s = input.read_into_vec().unwrap();
     assert_eq!(s.len(), 5);
+}
+
+#[test]
+fn can_read_into_array_exact() {
+    let bytes = b"hello";
+    let mut input = Cursor::new(bytes as &[u8]);
+    let s = input.read_into_array_exact::<5>().unwrap();
+    assert_eq!(s, [b'h', b'e', b'l', b'l', b'o']);
+    let s = input.read_into_array_exact::<4>();
+    assert!(s.is_err());
+    let s = input.read_into_array_exact::<6>();
+    assert!(s.is_err());
 }
